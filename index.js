64c3a88bc1febb918b976c824e5e7beb7354db9a
adebello04 <53411077+adebello04@users.noreply.github.com>
2021-02-04 14:55:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Telegraf = require('telegraf')
-const bot = new Telegraf()
+const bot = new Telegraf('1408289458:AAGKFTS5H4YjLF4Z1SSH0wFN-lM1FOMcOvM')
 
 bot.telegram.deleteWebhook().then(success => {
   success && console.log('🤖 is listening to your commands')
